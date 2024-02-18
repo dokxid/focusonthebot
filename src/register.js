e6d87321af88c7026f157cf9bf4127b916ed38de
dokxid <dan-du@live.de>
2024-02-18 20:44:07 +0100

    style: add comments, detailing the code

--- src/register.js
+++ src/register.js
@@ -2,14 +2,14 @@ import { ENABLE_CHANNEL_COMMAND, MESSAGES_COMMAND } from './commands.js';
 import dotenv from 'dotenv';
 import process from 'node:process';
 
-// load .dev.vars
-var args = process.argv.slice(2);
+// load .dev.vars and exec arguments
+const args = process.argv.slice(2);
 console.log(args[0] ? `detected argument: ${args}` : 'no args detected');
 dotenv.config({ path: '.dev.vars' });
 
+// assign correct variables according to exec argument
 let token = '';
 let applicationId = '';
-
 switch (args[0]) {
   // case: "node src/register.js production" get prod env vars
   case 'production':
@@ -31,7 +31,7 @@ switch (args[0]) {
     break;
 }
 
-// catch empty vars
+// catch empty vars and throw an error
 if (!token) {
   throw new Error('The DISCORD_TOKEN environment variable is required.');
 }
@@ -41,14 +41,15 @@ if (!applicationId) {
   );
 }
 
+// constants
+const url_api = `https://discord.com/api/v10/applications/${applicationId}`;
+const url = url_api + `/commands`;
 const commands = JSON.stringify([MESSAGES_COMMAND, ENABLE_CHANNEL_COMMAND]);
 
 /**
  * Register all commands globally.  This can take o(minutes), so wait until
  * you're sure these are the commands you want.
  */
-const url_api = `https://discord.com/api/v10/applications/${applicationId}`;
-const url = url_api + `/commands`;
 
 const response = await fetch(url, {
   headers: {
@@ -61,8 +62,6 @@ const response = await fetch(url, {
 
 if (response.ok) {
   console.log('Registered all commands');
-  // const data = await response.json();
-  // console.log(JSON.stringify(data, null, 2));
 } else {
   console.error('Error registering commands');
   let errorText = `Error registering commands \n ${response.url}: ${response.status} ${response.statusText}`;
